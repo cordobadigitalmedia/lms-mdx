983dada014d79d0691d86082740060473905d0c5
Cordoba <ismael@cordobadigitalmedia.com>
2022-02-11 11:56:03 +0200

    added MDX remote viewer

--- pages/old-courses/[course]/index.js
+++
@@ -1,7 +0,0 @@
-import { useRouter } from 'next/router'
-
-export default function Course() {
-  const router = useRouter()
-  const { course } = router.query
-  return <div className="prose prose-green"><h1>{course}</h1></div>;
-}
